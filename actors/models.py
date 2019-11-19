bf42d022626c08aec779c59752cb704408fc0830
Ali Asghar <ali@crymzee.com>
2019-11-19 12:58:52 +0500

    resolved string

--- actors/models.py
+++ actors/models.py
@@ -20,6 +20,9 @@ class User(AbstractUser):
     name = models.CharField(max_length=50,null=True)
     contact = models.CharField(max_length=50,null=True)
     image = models.ImageField(upload_to='pics/', blank=True)
+
+    def __str__(self):
+        return self.username
  
 
 
--- orders/models.py
+++ orders/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from actors.models import Customer,User
 from items.models import Item
+from datetime import datetime
 
 class Order(models.Model):
-    datetime = models.DateTimeField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     #customer_id = models.ForeignKey(Customer, on_delete = models.CASCADE,)
-    user_id = models.ForeignKey(User, on_delete = models.CASCADE,)
-    payment_method = models.CharField(max_length=50,null=True)
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE,blank=True, null=True)
+    payment_method = models.CharField(max_length=50,blank=True, null=True)
     # order_item_id = models.ForeignKey(Order_item, related_name='order', on_delete = models.CASCADE,)
 
+    
+
 class Order_item(models.Model):
-    item = models.ForeignKey(Item, on_delete = models.CASCADE,)
-    quantity = models.IntegerField(null=True)
-    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete = models.CASCADE,blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    order = models.ForeignKey(Order, related_name='order_items', on_delete=models.CASCADE,blank=True, null=True)
 
     class Meta:
         unique_together = ['item', 'order']
 
 class Order_detail(models.Model):
-    order_item_id = models.ForeignKey(Order_item, on_delete = models.CASCADE,)
-    order_id = models.ForeignKey(Order, related_name='order', on_delete = models.CASCADE,)
+    order_item_id = models.ForeignKey(Order_item, on_delete = models.CASCADE,blank=True, null=True)
+    order_id = models.ForeignKey(Order, related_name='order', on_delete = models.CASCADE,blank=True, null=True)
