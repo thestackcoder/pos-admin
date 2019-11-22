2ecf0a95c0e966e5e85f2fc7ceeedb690ee54a4b
Ali Asghar <ali@crymzee.com>
2019-11-22 10:09:43 +0500

    user-image

--- actors/admin.py
+++ actors/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Customer,User
-
-admin.site.register(Customer)
-admin.site.register(User)
+# admin.site.register(Customer)
+# admin.site.register(User)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
