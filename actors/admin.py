bf1f2c57c257a3200afbfda462035a84229a7438
Fahad Ahmed <fahadahmed2496@gmail.com>
2019-12-12 09:41:35 +0500

    admin.py conflict

--- actors/admin.py
+++ actors/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Customer,User
-<<<<<<< HEAD
 
 admin.site.register(Customer)
 
@@ -8,13 +7,5 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'name', 'is_staff')
     list_filter = ('is_active',)
 
-=======
-# admin.site.register(Customer)
-# admin.site.register(User)
-class CustomerAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Customer, CustomerAdmin)
-class UserAdmin(admin.ModelAdmin):
-    pass
->>>>>>> 2ecf0a95c0e966e5e85f2fc7ceeedb690ee54a4b
+
 admin.site.register(User, UserAdmin)
