b616f548afd7fe76a1874b3224992673bdf50333
Fahad Ahmed <fahadahmed2496@gmail.com>
2019-12-13 12:09:57 +0500

    customer RFID

--- orders/serializers.py
+++ orders/serializers.py
@@ -16,11 +16,12 @@ class Order_itemSeriaizer(serializers.ModelSerializer):
 class OrderSeriaizer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user_id.name',required=False)
     customer_name = serializers.CharField(source='customer.name',required=False)
+    customer_rfid = serializers.CharField(source='customer.RF_id',required=False)
     datetime = serializers.DateTimeField(format="%b %d,%Y %H:%M%p", read_only=True)
     order_items = Order_itemSeriaizer(many=True)
     class Meta:
         model = Order
-        fields = ['id', 'datetime','user_id','user_name', 'customer', 'customer_name', 'payment_method','order_items']
+        fields = ['id', 'datetime','user_id', 'user_name', 'customer_rfid', 'customer_name', 'payment_method','order_items']
 
     def create(self, validated_data):
         order_items = validated_data.pop('order_items')
--- reports/views.py
+++ reports/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from orders.models import Order
-
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render_to_response
 # from .forms import ItemReport
 
 def get_item_report(request):
-    start_date = request.POST.get('start')
-    end_date = request.POST.get('end')
+    start_date = request.POST.get('start_date')
+    end_date = request.POST.get('end_date')
+
+    print(start_date)
+    print(end_date)
 
     order_items = Order.objects.filter(datetime__range=[start_date, end_date])
+    print(order_items)
 
     return HttpResponse(order_items)
 
