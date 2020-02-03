fae42b44353690c40efabeaddf41621b40c285bb
Fahad Ahmed <35034320+thestackcoder@users.noreply.github.com>
2020-02-03 20:21:37 +0500

    change max, avg and min price

--- orders/views.py
+++ orders/views.py
@@ -68,13 +68,13 @@ def orders(request):
     orders = Order.objects.all()
     total_orders = Order.objects.count()
     
-    avg_order_price = Order.objects.all().aggregate(Avg("order_items__item__price"))
+    avg_order_price = Order.objects.aggregate(Avg("order_items__item__price"))
     #avg_price = round(avg_order_price.get('order_items__item__price__avg'),1)
     
-    max_order_price = Order.objects.all().aggregate(Max("order_items__item__price"))
+    max_order_price = Order.objects.aggregate(Max("order_items__item__price"))
     #max_price = round(max_order_price.get('order_items__item__price__max'),1)
     
-    min_order_price = Order.objects.all().aggregate(Min("order_items__item__price"))
+    min_order_price = Order.objects.aggregate(Min("order_items__item__price"))
     #min_price = round(min_order_price.get('order_items__item__price__min'),1)
     # last week order graph 
     # today = date.today()
